2bae0628f18f97623366d7c9ead23c72d3e1a4f8
Gabriel Rodrigues <38117500+Gabriel1011@users.noreply.github.com>
2021-07-25 11:32:31 -0300

    Alterando para .Net 5

--- Neo.DataBase/Helpers/Extensions/StringExtensions.cs
+++ Neo.DataBase/Helpers/Extensions/StringExtensions.cs
@@ -9,19 +9,19 @@ namespace Neo.DataBaseHelpers.Extensions
                 await Task.Run(() => { if (!Directory.Exists(path)) Directory.CreateDirectory(path); });
 
         public static async Task CheckAndCreateFileAsync(this string fileName, string directory) =>
-           await Task.Run(() => { if (!File.Exists(directory + fileName)) File.Create(directory + fileName).DisposeAsync(); });
+          await Task.Run(() => { if (!File.Exists(directory + fileName)) File.Create(directory + fileName).DisposeAsync(); });
 
         public static void CheckAndCreateDirectory(this string path)
         {
-            if (!Directory.Exists(path)) 
+            if (!Directory.Exists(path))
                 Directory.CreateDirectory(path);
-        }                
+        }
 
-        public static void CheckAndCreateFile(this string fileName, string directory) 
+        public static void CheckAndCreateFile(this string fileName, string directory)
         {
             if (!File.Exists(directory + fileName))
                 (File.Create(directory + fileName)).Dispose();
-        } 
+        }
 
         public static bool IsNullOrEmpty(this string value) => string.IsNullOrEmpty(value);
 
